83c2be3ba971d05030ae4705b6904f3fa3e0958f
JonasDevDE <73913242+JonasDevDE@users.noreply.github.com>
2023-04-24 19:40:39 +0200

    feat: re-added some stuff

--- src/main/java/de/igweb/igtaskchains/Task.java
+++ src/main/java/de/igweb/igtaskchains/Task.java
@@ -17,6 +17,62 @@ public class Task {
         this.runnable = runnable;
     }
 
+    public Task(Chain chain, String id, Runnable runnable) {
+        this(chain, id, 0, runnable);
+    }
+
+    public Task(Chain chain, Runnable runnable) {
+        this(chain, null, 0, runnable);
+    }
+
+    public Task(Chain chain, int priority, Runnable runnable) {
+        this(chain, null, priority, runnable);
+    }
+
+    public Task(Chain chain, String id, int priority) {
+        this(chain, id, priority, null);
+    }
+
+    public Task(Chain chain, String id) {
+        this(chain, id, 0, null);
+    }
+
+    public Task(Chain chain, int priority) {
+        this(chain, null, priority, null);
+    }
+
+    public Task(Chain chain) {
+        this(chain, null, 0, null);
+    }
+
+    public Task(String id, int priority, Runnable runnable) {
+        this(null, id, priority, runnable);
+    }
+
+    public Task(String id, Runnable runnable) {
+        this(null, id, 0, runnable);
+    }
+
+    public Task(int priority, Runnable runnable) {
+        this(null, null, priority, runnable);
+    }
+
+    public Task(Runnable runnable) {
+        this(null, null, 0, runnable);
+    }
+
+    public Task(String id, int priority) {
+        this(null, id, priority, null);
+    }
+
+    public Task(String id) {
+        this(null, id, 0, null);
+    }
+
+    public Task(int priority) {
+        this(null, null, priority, null);
+    }
+
     /**
      * @return the chain of the task
      */
